3610ddc025acbd932c90b2b206f9b65f5108670c
Cristian Medina <cdmedina229@gmail.com>
2025-07-20 22:18:56 -0500

    update login

--- auth/app.py
+++ auth/app.py
@@ -77,7 +77,8 @@ def login():
         'token': token,
         'date': datetime.datetime.now().isoformat() + 'Z',
         'expires_at': payload['exp'].isoformat() + 'Z',
-        'id_user': user['id_user']
+        'id_user': user['id_user'],
+        'email': user['email'],
     }), 200
 
 @app.route('/')
